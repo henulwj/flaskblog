31a046d86d84bf848a99d0d014103d77ace08693
henulwj <henulwj@qq.com>
2015-10-22 22:43:08 +0800

    add templates index.html user.html

---
+++ flaskblog.py
@@ -0,0 +1,19 @@
+#　_*_ coding:utf-8 _*_
+
+from flask import Flask, request, current_app, g, render_template
+from flask.ext.script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+if __name__ == '__main__':
+    manager.run()
