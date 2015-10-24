60c6b065e33150481383c131dab329409ee25ab3
henulwj <henulwj@qq.com>
2015-10-24 16:34:43 +0800

    user flask-sqlalchemy and flask-migration

--- flaskblog.py
+++ flaskblog.py
@@ -1,43 +1,87 @@
-#　_*_ coding:utf-8 _*_
+# 　_*_ coding:utf-8 _*_
 # encoding = utf-8
-import sys
+import sys, os
 from datetime import datetime
 
 from flask import Flask, request, current_app, g, render_template, session, redirect, url_for, flash
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired  #Require将过期
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired  # Require将过期
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import  Migrate, MigrateCommand
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+# 获取当前项目路径
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+# flask-wtf设置的表单密钥，用来验证CSRF
 app.config["SECRET_KEY"] = "hello world"
+# 配置数据库路径
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# 每次请求结束后自动提交数据库中的变动
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 class NameForm(Form):
     name = StringField("姓名：", validators=[DataRequired()])
     submit = SubmitField("提交")
 
 
+class Role(db.Model):
+
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship("User", backref="role", lazy="dynamic")
+
+    def __str__(self):
+        return '<Role %r>' % self.name
+
+    __repr__ = __str__
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __str__(self):
+        return '<User %r>' % self.username
+
+    __repr__ = __str__
+
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash("用户名改变")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data, role_id=3)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
-        # form.name.data = ""
         return redirect(url_for('index'))
-    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'), known = session.get('known', False))
 
 
 @app.route('/user/<name>')
@@ -47,12 +91,13 @@ def user(name):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"),404
+    return render_template("404.html"), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template("500.html"),500
+    return render_template("500.html"), 500
+
 
 if __name__ == '__main__':
     manager.run()
