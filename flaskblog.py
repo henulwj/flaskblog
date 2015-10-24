4955e518ef2bb4beff072f6fcad6d90668b6005f
henulwj <henulwj@qq.com>
2015-10-24 10:16:19 +0800

    use jinja and add favicon

--- flaskblog.py
+++ flaskblog.py
@@ -2,10 +2,11 @@
 
 from flask import Flask, request, current_app, g, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -14,6 +15,14 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"),500
 
 if __name__ == '__main__':
     manager.run()
