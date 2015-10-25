a018132db33180c2d53f8a54322a1107b6506419
henulwj <henulwj@qq.com>
2015-10-25 10:46:31 +0800

    use flask-mail

--- flaskblog.py
+++ flaskblog.py
@@ -2,6 +2,7 @@
 # encoding = utf-8
 import sys, os
 from datetime import datetime
+from threading import Thread
 
 from flask import Flask, request, current_app, g, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager, Shell
@@ -12,6 +13,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired  # Require将过期
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import  Migrate, MigrateCommand
+from flask.ext.mail import Mail, Message
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -26,17 +28,45 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 # 每次请求结束后自动提交数据库中的变动
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
+app.config['MAIL_SERVER'] = 'smtp.sina.com'
+app.config['MAIL_PORT'] = 25
+# app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+app.config['FLASKBLOG_MAIL_SUBJECT_PREFIX'] = '[FLASKBLOG]'
+app.config['FLASKBLOG_MAIL_SENDER'] = 'henulwj@sina.com'
+app.config['FLASKBLOG_ADMIN'] = 'henulwj@qq.com'
+
+
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKBLOG_MAIL_SUBJECT_PREFIX']+subject, sender=app.config['FLASKBLOG_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template+'.txt', **kwargs)
+    msg.html = render_template(template+'.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
 
 class NameForm(Form):
     name = StringField("姓名：", validators=[DataRequired()])
@@ -77,6 +107,8 @@ def index():
             user = User(username=form.name.data, role_id=3)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKBLOG_ADMIN']:
+                send_mail(app.config['FLASKBLOG_ADMIN'], '新用户', 'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
